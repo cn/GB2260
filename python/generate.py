0c959b87b49d8061522f69784b1dc2feeeec6041
Jiangge Zhang <tonyseek@gmail.com>
2014-12-10 18:18:39 +0800

    support to generate source from multiple files.

--- python/generate.py
+++ python/generate.py
@@ -6,32 +6,44 @@ A script to generate the data module.
 
 from __future__ import print_function, unicode_literals
 
+import os
 import sys
 import pprint
 
 
+def get_name_suffix(path):
+    path, _ = os.path.splitext(path)
+    splited = path.rsplit('-', 1)
+    if len(splited) == 2:
+        return splited[-1]
+    return ''
+
+
 def main():
-    try:
-        _, source, destination = sys.argv
-    except ValueError:
-        print('Usage: {.argv[0]} [SOURCE] [DESTINATION]'.format(sys),
+    if len(sys.argv) < 3:
+        print('Usage: {.argv[0]} [A] [B] ... [DESTINATION]'.format(sys),
               file=sys.stderr)
         sys.exit(1)
+    source = sys.argv[1:-1]
+    destination = sys.argv[-1]
 
     data = {}
+    for current_source in source:
+        suffix = get_name_suffix(current_source)
+        current_dict = data.setdefault(int(suffix) if suffix else None, {})
 
-    with open(source, 'r') as source_file:
-        for line in source_file:
-            code, name = line.strip().split()
-            data[int(code)] = name.decode('utf-8')
+        with open(current_source, 'r') as source_file:
+            for line in source_file:
+                code, name = line.strip().split()
+                current_dict[int(code)] = name.decode('utf-8')
 
-    items = pprint.pformat(data, indent=4).strip('{}')
-    result = '\n'.join(['data = {', ' {0} '.format(items), '}'])
-
+    result = 'data = {0}'.format(repr(data))
     with open(destination, 'w') as destination_file:
         print(result, file=destination_file)
 
-    print('{0} records has been generated.'.format(len(data)), file=sys.stderr)
+    for current_dict in data.values():
+        message = '{0} records has been generated.'.format(len(current_dict))
+        print(message, file=sys.stderr)
 
 
 if __name__ == '__main__':
