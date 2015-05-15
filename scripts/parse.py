cf2a6e97a5bd81f3c1045faddf662a3f59869c8c
Jiangge Zhang <tonyseek@gmail.com>
2015-05-15 16:08:53 +0800

    collect all data since 2012.

--- scripts/parse.py
+++ scripts/parse.py
@@ -5,6 +5,7 @@ from __future__ import print_function
 import os
 import re
 import sys
+import itertools
 
 import requests
 from lxml.html import fromstring
@@ -12,19 +13,31 @@ from lxml.html import fromstring
 
 URL_BASE = 'http://www.stats.gov.cn/tjsj/tjbz/xzqhdm/'
 URL_LIST = [
-    ('', '201401/t20140116_501070.html'),  # latest: 2013
-    ('-2012', '201301/t20130118_38316.html'),
-    ('-2011', '201201/t20120105_38315.html'),
-    ('-2010', '201107/t20110726_38314.html'),
-    ('-2009', '201006/t20100623_38313.html'),
-    ('-2008', '200906/t20090626_38312.html'),
-    ('-2007', '200802/t20080215_38311.html'),
-    ('-2006', '200704/t20070411_38310.html'),
+    # (year, url, is_mass)
+    ('', '201401/t20140116_501070.html', False),
+    ('-2012', '201301/t20130118_38316.html', False),
+    ('-2011', '201201/t20120105_38315.html', False),
+    ('-2010', '201107/t20110726_38314.html', False),
+    ('-2009', '201006/t20100623_38313.html', False),
+    ('-2008', '200906/t20090626_38312.html', False),
+    ('-2007', '200802/t20080215_38311.html', False),
+    ('-2006', '200704/t20070411_38310.html', False),
+    ('-2005', '200410/t20041022_38307.html', True),
+    ('-200506', '200410/t20041022_38306.html', True),
+    ('-2004', '200410/t20041022_38305.html', True),
+    ('-200409', '200410/t20041022_38304.html', True),
+    ('-200403', '200406/t20040607_38302.html', True),
+    ('-2003', '200402/t20040211_38301.html', True),
+    ('-200306', '200307/t20030722_38300.html', True),
+    ('-2002', '200302/t20030219_38299.html', True),
 ]
 
 XPATH_EXPRS = [
     './/div[@class="xilan_con"]//tbody/tr',
     './/div[@class="xilan_con"]//p[not(ancestor::tbody)]',
+]
+XPATH_MASS_EXPRS = [
+    './/p[@class="MsoNormal"]//span//text()',
 ]
 
 
@@ -39,7 +52,12 @@ def strip_comments(line):
     return line.strip('*() \n\t\r').strip()
 
 
-def iter_lines(element):
+def iter_lines(element, is_mass):
+    if is_mass:
+        for line in iter_lines_of_mass_document(element):
+            yield line
+        return
+
     for xpath in XPATH_EXPRS:
         line_elements = element.xpath(xpath)
         for el in line_elements:
@@ -51,20 +69,38 @@ def iter_lines(element):
                 yield u' '.join(fragments)
 
 
+def iter_lines_of_mass_document(element):
+    for xpath in XPATH_MASS_EXPRS:
+        fragments = [e.strip() for e in element.xpath(xpath)]
+        stashed_fragments = []
+        for fragment, next_fragment in predict(fragments):
+            stashed_fragments.append(fragment)
+            if next_fragment and next_fragment.isdigit() and stashed_fragments:
+                yield '\t'.join(f for f in stashed_fragments if f)
+                stashed_fragments = []
+        yield '\t'.join(stashed_fragments)
+
+
+def predict(iterable):
+    collection = list(iterable)
+    return itertools.izip_longest(collection, collection[1:])
+
+
 def main():
     if len(sys.argv) != 2:
         print('Usage: %s [dir]' % sys.argv[0], file=sys.stderr)
         sys.exit(0)
 
-    for suffix, url in URL_LIST:
+    for suffix, url, is_mass in URL_LIST:
         req = requests.get(URL_BASE + url)
         req.encoding = 'utf-8'
         el = fromstring(req.text)
 
         dirname = os.path.join(sys.argv[1], 'GB2260%s.txt' % suffix)
+        print('--> %s' % dirname, file=sys.stderr)
 
         with open(dirname, 'w') as dest_file:
-            for line in iter_lines(el):
+            for line in iter_lines(el, is_mass):
                 text = strip_spaces_in_chinese_words(strip_comments(line))
                 if not text:
                     continue
