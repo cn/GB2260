6ffc978d88064cd2a0e55caefe294e4ba913a87c
Hsiaoming Yang <me@lepture.com>
2015-09-07 17:29:11 +0800

    Always use suffix

--- scripts/parse.py
+++ scripts/parse.py
@@ -15,23 +15,23 @@ from lxml.html import fromstring
 URL_BASE = 'http://www.stats.gov.cn/tjsj/tjbz/xzqhdm/'
 URL_LIST = [
     # (year, url, is_mass)
-    ('', '201504/t20150415_712722.html', False),
-    ('-2013', '201401/t20140116_501070.html', False),
-    ('-2012', '201301/t20130118_38316.html', False),
-    ('-2011', '201201/t20120105_38315.html', False),
-    ('-2010', '201107/t20110726_38314.html', False),
-    ('-2009', '201006/t20100623_38313.html', False),
-    ('-2008', '200906/t20090626_38312.html', False),
-    ('-2007', '200802/t20080215_38311.html', False),
-    ('-2006', '200704/t20070411_38310.html', False),
-    ('-2005', '200410/t20041022_38307.html', True),
-    ('-200506', '200410/t20041022_38306.html', True),
-    ('-2004', '200410/t20041022_38305.html', True),
-    ('-200409', '200410/t20041022_38304.html', True),
-    ('-200403', '200406/t20040607_38302.html', True),
-    ('-2003', '200402/t20040211_38301.html', True),
-    ('-200306', '200307/t20030722_38300.html', True),
-    ('-2002', '200302/t20030219_38299.html', True),
+    (2014, '201504/t20150415_712722.html', False),
+    (2013, '201401/t20140116_501070.html', False),
+    (2012, '201301/t20130118_38316.html', False),
+    (2011, '201201/t20120105_38315.html', False),
+    (2010, '201107/t20110726_38314.html', False),
+    (2009, '201006/t20100623_38313.html', False),
+    (2008, '200906/t20090626_38312.html', False),
+    (2007, '200802/t20080215_38311.html', False),
+    (2006, '200704/t20070411_38310.html', False),
+    (2005, '200410/t20041022_38307.html', True),
+    (200506, '200410/t20041022_38306.html', True),
+    (2004, '200410/t20041022_38305.html', True),
+    (200409, '200410/t20041022_38304.html', True),
+    (200403, '200406/t20040607_38302.html', True),
+    (2003, '200402/t20040211_38301.html', True),
+    (200306, '200307/t20030722_38300.html', True),
+    (2002, '200302/t20030219_38299.html', True),
 ]
 
 XPATH_EXPRS = [
@@ -98,7 +98,7 @@ def main():
         req.encoding = 'utf-8'
         el = fromstring(req.text)
 
-        dirname = os.path.join(sys.argv[1], 'GB2260%s.txt' % suffix)
+        dirname = os.path.join(sys.argv[1], 'GB2260-%s.txt' % suffix)
         print('--> %s' % dirname, file=sys.stderr)
 
         with open(dirname, 'w') as dest_file:
